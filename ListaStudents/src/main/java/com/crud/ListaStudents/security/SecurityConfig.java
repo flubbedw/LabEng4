ecd6ceb1eefe2fee8d04cf737cbffc1a94026c6c
Julio Vanderlei <chartsw@hotmail.com>
2023-12-01 20:17:55 +0000

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   Carrinho_A1/target/classes/META-INF/persistence.xml 	modified:   ListaStudents/pom.xml 	new file:   ListaStudents/src/main/java/com/crud/ListaStudents/security/SecurityConfig.java

---
+++ ListaStudents/src/main/java/com/crud/ListaStudents/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.crud.ListaStudents.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("teste")
+                .password(passwordEncoder().encode("senhasupersecreta"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
